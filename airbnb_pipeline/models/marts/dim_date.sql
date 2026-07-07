e694654d63c6c2b7fd8393f4309911f9320c7260
bananya <bpgayathriananya@gmail.com>
2026-07-07 14:34:44 -0500

    both facts

--- airbnb_pipeline/models/marts/dim_date.sql
+++ airbnb_pipeline/models/marts/dim_date.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 with spine as (
-    {{ dbt_utils.date_spine(depart='day',
-        start_date="to_date('2010-01-01')"
+    {{ dbt_utils.date_spine(datepart="day",
+        start_date="to_date('2010-01-01')",
         end_date="dateadd(day, 400, current_date)") }}
 )
 
@@ -10,7 +10,7 @@ select
     year(date_day) as year,
     quarter(date_day) as quarter,
     month(date_day) as month_num,
-    to_cahr(date_day, 'MMMM') as month_name,
-    dayofweek(date_day) as day_of _week,
+    to_char(date_day, 'MMMM') as month_name,
+    dayofweek(date_day) as day_of_week,
     dayofweek(date_day) in (0, 6) as is_weekend
 from spine
